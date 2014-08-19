ae6c2a6567734ba1c299ce3ce7faf187ae975469
Kia Fathi <kiafathi@me.com>
2014-08-19 14:59:23 -0700

    Initial Commit

---
+++ MyFirstApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstApplication
+//
+//  Created by Kia Fathi on 8/19/14.
+//  Copyright (c) 2014 Kia Fathi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirstApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstApplication
+//
+//  Created by Kia Fathi on 8/19/14.
+//  Copyright (c) 2014 Kia Fathi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
